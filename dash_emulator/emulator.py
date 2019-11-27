b643a55482ae31f7cf87b13a4a6e71de50b55ee0
Yang Liu <ly950710@gmail.com>
2019-11-27 13:29:04 -0700

    Complete MPD parsing
    Signed-off-by: Yang Liu <ly950710@gmail.com>

--- dash_emulator/emulator.py
+++ dash_emulator/emulator.py
@@ -1,15 +1,14 @@
 import requests
 
-from dash_emulator import arguments
+from dash_emulator import arguments, mpd
 
 
 class Emulator():
     def __init__(self, args):
         self.args = args
-
-    def MPD_parser(self, content):
-        pass
+        self.mpd = None
 
     def start(self):
         target: str = self.args[arguments.PLAYER_TARGET]  # MPD file link
         mpd_content: str = requests.get(target).text
+        self.mpd = mpd.MPD(mpd_content, target)
--- dash_emulator/mpd.py
+++ dash_emulator/mpd.py
@@ -1,17 +1,38 @@
+import re
+from urllib.parse import urlparse
+import os.path
 from typing import List, Optional
+import xml.etree.ElementTree as ET
+from dash_emulator.logger import getLogger
+
+log = getLogger(__name__)
 
 
-class Representation():
-    def __init__(self, id, bandwidth, width, height, codec):
-        self._id = id
-        self._bandwidth = bandwidth
-        self._width = width
-        self._height = height
-        self._codec = codec
+class Representation(object):
+    def __init__(self, tree, adapatationSet):
+        self._id = None
+        self._bandwidth = None
+        self._width = None
+        self._height = None
+        self._mime = None
+        self._codec = None
+
+        self._adapatationSet = adapatationSet  # type: AdapationSet
+
+        self.initialization = None
+        self.media = None
+        self.startNumber = None
+
+        self.durations = None  # type: Optional[List[float]]
+        self.urls = None  # type: Optional[List[str]]
+
+        self.baseurl = None
+
+        self.parse(tree)
 
     @property
     def id(self):
-        return self.id
+        return self._id
 
     @property
     def bandwidth(self):
@@ -29,18 +50,114 @@ class Representation():
     def codec(self):
         return self._codec
 
+    def parse(self, tree):
+        base = urlparse(self._adapatationSet.mpd.link)
+        basepath = os.path.dirname(base.path)
 
-class MPD():
-    def __init__(self, content: str):
+        self._id = tree.attrib['id']
+        self._mime = tree.attrib['mimeType']
+        self._codec = tree.attrib['codecs']
+        self._bandwidth = tree.attrib['bandwidth']
+        self._width = tree.attrib['width']
+        self._height = tree.attrib['height']
+
+        segmentTemplate = tree.find("{%s}SegmentTemplate" % self._adapatationSet.mpd.namespace)
+        self.initialization = segmentTemplate.attrib["initialization"].replace("$RepresentationID$",
+                                                                               str(self.id))
+        self.initialization = base._replace(path=os.path.join(basepath, self.initialization)).geturl()
+        self.media = segmentTemplate.attrib['media']
+        self.startNumber = int(segmentTemplate.attrib['startNumber'])
+
+        self.durations = [None] * self.startNumber
+        self.urls = [None] * self.startNumber
+
+        segmentTimeline = segmentTemplate.find("{%s}SegmentTimeline" % self._adapatationSet.mpd.namespace)
+
+        num = self.startNumber
+        for segment in segmentTimeline:
+            self.durations.append(segment.attrib['d'])
+            filename = re.sub(r"\$Number(\%\d+d)\$", r"\1", self.media.replace("$RepresentationID$", str(self.id)))
+            filename = filename % num
+            self.urls.append(base._replace(path=os.path.join(basepath, filename)).geturl())
+
+            if 'r' in segment.attrib:
+                for i in range(int(segment.attrib['r'])):
+                    num += 1
+                    self.durations.append(segment.attrib['d'])
+                    filename = re.sub(r"\$Number(\%\d+d)\$", r"\1",
+                                      self.media.replace("$RepresentationID$", str(self.id)))
+                    filename = filename % num
+                    self.urls.append(base._replace(path=os.path.join(basepath, filename)).geturl())
+
+            num += 1
+
+        print(self.urls)
+
+
+class AdapationSet(object):
+    def __init__(self, tree, mpd):
+        self.tree = tree
+        self.id = None  # type: Optional[int]
+        self.representations = []  # type: List[Representation]
+        self.mpd = mpd  # type: MPD
+
+        self.parse()
+
+    def parse(self):
+        self.id = self.tree.attrib['id']
+        for representation in self.tree:
+            self.representations.append(Representation(representation, self))
+
+
+class MPD(object):
+    def __init__(self, content: str, link):
         self.content = content
+        self.link = link
 
-        self.mediaPresentationDuration = None  # type: Optional[int]
+        self.type = None
+        self.namespace = None
+        self.mediaPresentationDuration = None  # type: Optional[float]
         self.minBufferTime = None  # type: Optional[int]
-        self.videoRepresentations = []  # type: List[Representation]
-        self.audioRepresentations = []  # type: List[Representation]
+        self.videoAdaptationSet = None  # type: Optional[AdapationSet]
+        self.audioAdaptationSet = None  # type: Optional[AdapationSet]
 
-        self.parse(self.content)
+        self.parse()
 
-    def parse(self, content):
-        # TODO
-        pass
+    def parse(self):
+        def ISO8601TimeParse(duration):
+            pattern = r"^PT(?:(\d+(?:.\d+)?)H)?(?:(\d+(?:.\d+)?)M)?(?:(\d+(?:.\d+)?)S)?$"
+            results = re.match(pattern, duration)
+            dur = [float(i) if i is not None else 0 for i in results.group(1, 2, 3)]
+            dur = 3600 * dur[0] + 60 * dur[1] + dur[2]
+            return dur
+
+        root = ET.fromstring(self.content)
+        pattern = r"^\{([\s\S]+)\}[\s\S]+$"
+        self.namespace = re.match(pattern, root.tag).group(1)
+
+        # type
+        self.type = root.attrib["type"]
+
+        # mediaPresentationDuration
+        if "mediaPresentationDuration" in root.attrib:
+            mediaPresentationDuration = root.attrib["mediaPresentationDuration"]
+            self.mediaPresentationDuration = ISO8601TimeParse(mediaPresentationDuration)
+
+        # minBuffer
+        if "minBufferTime" in root.attrib:
+            minBufferTime = root.attrib["minBufferTime"]
+            self.minBufferTime = ISO8601TimeParse(minBufferTime)
+
+        period = root.find("{%s}Period" % self.namespace)
+        if period is None:
+            log.error("[Parse MPD] Cannot find \"Period\" tag")
+            exit(1)
+
+        for adapationSet in period:
+            contentType = adapationSet.attrib["contentType"]
+            if contentType == 'video':
+                self.videoAdaptationSet = AdapationSet(adapationSet, self)
+            elif contentType == 'audio':
+                self.audioAdaptationSet = AdapationSet(adapationSet, self)
+            else:
+                log.error("[Parse MPD] Unsupported content type in AdaptationSet: %s" % contentType)
